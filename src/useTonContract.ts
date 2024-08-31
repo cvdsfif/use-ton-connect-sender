2a0a5c44051a2a6028c643fdd5d537385b900983
Nikit Zykov <nikit@zykov.com>
2024-08-31 08:57:48 +0200

    Renaming package

--- src/useTonContract.ts
+++ src/useTonContract.ts
@@ -15,13 +15,9 @@ export const useTonContract = <T extends Contract>(
     const client = useTonClient(network)
     const contract = useAsyncInitialize(async () => {
         if (!client) return
-        console.log(`Parsing address: {${unparsedAddress}}`)
         const address = Address.parse(unparsedAddress.toString())
-        console.log(`Address parsed: {${address.toString()}}`)
         const factoryClient = factory.fromAddress(address)
-        console.log(`Factory client`, factoryClient)
         const openedContract = client.open(factoryClient) as OpenedContract<T>
-        console.log(`Contract opened`, openedContract)
         return openedContract
     }, [client])
     return contract
